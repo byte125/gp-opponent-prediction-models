09bf8998e2b89d94eb42e2d6ad099fd9f6009d25
Shreya <sdeepak@andrew>
2024-04-25 21:46:39 -0400

    Add todo comments

--- racer_project/racer_project/gp_odometry_predictor.py
+++ racer_project/racer_project/gp_odometry_predictor.py
@@ -124,7 +124,8 @@ class GPOdometryPredictor(Node):
 
         # Get the ego vehicle's predicted path using MPCC
         # TODO: Fill the ego_pred object with the MPCC prediction
-        ego_pred = VehiclePrediction()
+        ego_pred = self.gp_mpcc_ego_controller.get_prediction()
+        print("EGO pred ", ego_pred)
         tv_pred = self.predictor.get_prediction(ego_state, tar_sim_state, ego_pred)
         print (tv_pred)
         # Perform prediction
